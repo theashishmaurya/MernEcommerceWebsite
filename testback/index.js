0138a71aada4a4f506d17fab0b37625b4c495263
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-06-01 21:40:46 +0530

    gitignore added

--- testback/index.js
+++ testback/index.js
@@ -16,13 +16,13 @@ app.get('/Login',(req,res) => {
 const admin = (req,res,)=>{
     res.send('This is admin page xD Dashboard');
 };
-
+// middleware function 
 const isAdmin = (res,req ,next ) => {
     console.log ("isAdmin working");
     next();
 }
 
-app.get('/admin' , isAdmin ,admin);
+app.get('/admin' , isAdmin ,admin); // isadmin is a middleware function and  admin is a function consedering req / res.
 
 app.get('/Signup',(req,res) => {
     res.send('Hello world , I am express and ur at Signup in page');
