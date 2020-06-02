92ffdfcfe4eb0ffab44789fca1d02071c3e8beea
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-06-02 11:22:26 +0530

    middleware added

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,10 +1,21 @@
 const mongoose = require ("mongoose");
 const express  = require ("express");
 require('dotenv').config() ;
+// Middleware Used 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
+// Connection mongoose database 
 mongoose.connect(process.env.DATABASE , {useNewUrlParser : true ,  useUnifiedTopology: true })
                 .then(() => console.log("MONGODB CONNECTED"))
                 .catch(()=>console.log("DATABASE NOT CONNECTED"))
+
+// Express server configuration
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 const port = 8000;
 const app = express();
