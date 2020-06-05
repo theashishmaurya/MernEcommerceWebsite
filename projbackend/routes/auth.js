1eb0e200c1685cfdbce224ecb7281fc13b06ac4a
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-06-05 14:28:55 +0530

    router validation added

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const Router =  express.Router()
 const {signout , signup} = require("../controllers/auth")
+const { check , validationResult} = require('express-validator');
 
+// Router and express validation
+Router.post('/signup' , [
+    check("name" ).isLength({ min: 2}).withMessage('name must be at least 2 characters long') , // validation for name
+    check("email").isEmail().withMessage('email is required'), // validation for email
+    check("password").isLength({min : 5}).withMessage("Password should be more than 5 characters") // validation for password
 
-Router.post('/signup' , signup );
+],signup );
 Router.get('/signout' , signout );
 
 module.exports = Router;
